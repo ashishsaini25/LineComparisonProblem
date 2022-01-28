0afc205c6fb7edad4cbda5e7b57366434b2ab328
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-01-28 11:55:08 +0530

    [Ashish] Add: Line Comparison Problem Solved

--- LineComaparisonProblem/Program.cs
+++ LineComaparisonProblem/Program.cs
@@ -12,13 +12,17 @@ namespace LineComaparisonProblem
         {
             Comparison line1 = new Comparison(3, 3, 3, 4);
             Comparison line2 = new Comparison(3, 2, 3, 4);
+            if (line1.Calculate() > line2.Calculate())
+            {
+                Console.WriteLine("Length of line1 is greater than line2\n");
+            }
+            if(line1.Calculate()<line2.Calculate())
+            {
+                Console.WriteLine("Length of line1 is Samller than line2\n");
+            }
             if (line1.Calculate() == line2.Calculate())
             {
-                Console.WriteLine("Both Lines Are Equal\n");
-            }
-            else
-            {
-                Console.WriteLine("Lines Are Not Equal\n");
+                Console.WriteLine("Both lines are Equal\n");
             }
         }
     }
