97b834f87f921ceb7d34c009a9340c07726e4e41
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-01-28 11:49:19 +0530

    [Ashish] Add: Eqaulity of Lines Problem Solved

--- LineComaparisonProblem/Comparison.cs
+++ LineComaparisonProblem/Comparison.cs
@@ -16,10 +16,10 @@ namespace LineComaparisonProblem
             this.x2 = x2;
             this.y2 = y2;
         }
-        public void Calculate()
+        public double Calculate()
         {
-             double length = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
-            Console.WriteLine("The Length of the Line is {0}", length);
+             return  Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
+            
         }
     }
 }
--- LineComaparisonProblem/Program.cs
+++ LineComaparisonProblem/Program.cs
@@ -10,8 +10,16 @@ namespace LineComaparisonProblem
     {
         static void Main(string[] args)
         {
-            Comparison comparison = new Comparison(1, 2, 3, 4);
-            comparison.Calculate();
+            Comparison line1 = new Comparison(3, 3, 3, 4);
+            Comparison line2 = new Comparison(3, 2, 3, 4);
+            if (line1.Calculate() == line2.Calculate())
+            {
+                Console.WriteLine("Both Lines Are Equal\n");
+            }
+            else
+            {
+                Console.WriteLine("Lines Are Not Equal\n");
+            }
         }
     }
 }
